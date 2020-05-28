a92bbd79d85bf84f49f1c5a0e84903a089030f7f
Andrea G <andreagrasso.ag@live.it>
2020-05-28 14:22:45 +0200

    add elements in produit.html

--- produits.js
+++ produits.js
@@ -2,15 +2,31 @@ let params = new URL(document.location).searchParams;
 let idArticle = params.get("id");
 console.log(idArticle);
 
-//attente reponse
-idArticle.onreadystatechange = function () {
+//creation et envoie objet requete
+let request = new XMLHttpRequest();
+request.open("GET", "http://localhost:3000/api/cameras/" + idArticle);
+request.send();
+
+//attente reponse et appel fonction de retour
+request.onreadystatechange = function () {
   if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
     // si tout c'est bien passée
-    let article = JSON.parse(this.responseText); // recuperation de la liste de produits
-    console.log(article);
+    let product = JSON.parse(this.responseText); // recuperation de la liste de produits
+    console.log(product.name); //appel de la fonction d'affichage des produits
+    showDetailsProduct(product);
   }
 };
 
-//const img = document.getElementById("img-article");
-//img.innerHTML = params[i].imageUrl;
-//console.log(idArticle.name);
+const showDetailsProduct = (article) => {
+  const name = document.getElementById("h3-article");
+  name.innerHTML = article.name;
+
+  const price = document.getElementById("price-article");
+  price.innerHTML = article.price + " €";
+
+  const description = document.getElementById("description-article");
+  description.innerHTML = article.description;
+
+  const img = document.getElementById("img-article");
+  img.src = article.imageUrl;
+};
